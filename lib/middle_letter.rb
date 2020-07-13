1ff13db4cd7b9af22c6ff52e9086fe369661bdf6
Ryan Viegas <rjkviegas@gmail.com>
2020-07-13 19:49:17 +0100

    third test for words with odd number lengths passed

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,5 +1,20 @@
 
 def get_middle(word)
-  "A"
+
+  if word.length % 2 == 0 
+    #even
+  else
+    #odd
+    middle = (word.length + 1)/ 2 - 1
+    word[middle]
+  end
+=begin
+  if word == "A"
+    "A"
+  else
+    "o"
+  end
+=end
+
 end
 
--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -2,6 +2,15 @@ require 'middle_letter'
 
 RSpec.describe do
   it 'return the middle letter of a word if the words length is odd' do
-  expect(get_middle("A")).to eq "A"
+    expect(get_middle("A")).to eq "A"
   end
+
+  it 'return the middle letter of a word if the words length is odd' do
+    expect(get_middle("Dog")).to eq "o"
+  end
+
+  it 'return the middle letter of a word if the words length is odd' do
+    expect(get_middle("Cat")).to eq "a"
+  end
+
 end
