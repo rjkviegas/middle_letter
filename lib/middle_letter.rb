666290be27c32206d9904e053ec5456c28c121c8
Ryan Viegas <rjkviegas@gmail.com>
2020-07-13 17:33:43 +0100

    passes test that checks whether word length is odd or even

---
+++ lib/middle_letter.rb
@@ -0,0 +1,4 @@
+
+def get_middle(word)
+  word.length % 2 == 0 ? true : false
+end
---
+++ spec/middle_letter_spec.rb
@@ -0,0 +1,15 @@
+require 'middle_letter'
+
+RSpec.describe do
+#    it 'tells me length of the word' do
+#        expect(get_middle("word")).to eq 4
+#    end
+
+    it 'returns true if word length is even' do
+        expect(get_middle("word")).to eq true
+    end
+
+    
+
+    
+end
